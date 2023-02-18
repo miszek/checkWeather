ad3ea4cd33cbad4ada9e8fbfe62f020a9da778ad
Michal Szekalski <szekalski.michal@gmail.com>
2023-02-18 16:07:33 +0100

    new project

---
+++ src/test/java/com/mszekalski/checkweather/CheckWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mszekalski.checkweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckWeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
